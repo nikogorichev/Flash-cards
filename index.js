e1808536763b8eeb56b8d7365bd2bedf7775d4c1
Nastasia03 <nastasia03@mail.ru>
2022-04-08 14:56:47 +0300

    Release Nastya

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ async function quest() {
   let arr = await fs.readdir('./topics')
   let newArr = arr.map((el, i) => `${i+1}) ${el.slice(0,-4)}`).join('\n')
   console.log(newArr)
+  let num = readline.question('Выбирай тему:\n')
+  let theme
+  for (let i = 0; i < arr.length; i++) {
+    if (num - 1 === i) {
+      theme = await fs.readFile(`./topics/${arr[i]}`, 'utf-8')
+    }
+  }
 }
